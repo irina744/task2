a6ac685b7c9a60651219c8a6b9363642d7282d98
irina744 <bel.iri.s@mail.ru>
2025-01-10 16:34:36 +0300

    dx

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -7,5 +7,4 @@ def func_template(request):
     return render(request, 'second_task/func_template.html')
 
 
-class class_template(TemplateView):
-    template_name = 'second_task/class_template.html'
+

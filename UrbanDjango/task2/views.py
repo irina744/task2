1674f39b7e5b9045bf7fa6abe7ad9547a8a1e09f
irina <bel.iri.s@mail.ru>
2025-01-10 17:15:53 +0300

    dz

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -6,5 +6,8 @@ from django.views.generic import TemplateView
 def func_template(request):
     return render(request, 'second_task/func_template.html')
 
+class class_template(TemplateView):
+    template_name = 'second_task/class_template.html'
+
 
 
